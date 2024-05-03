1ef62ce59031bb5d283a20166cb19efa5e7a2cd2
Sean Jackman <jackmansean64@gmail.com>
2024-05-03 10:00:37 -0700

    Modified file and image retrieval to crawl the provided source directory rather than assume the file structure.

--- obsidian-to-blog/obsidian-to-blog.py
+++ obsidian-to-blog/obsidian-to-blog.py
@@ -13,7 +13,12 @@ def _main(document_name):
     destination_folder = os.getenv('BLOG_DESTINATION_FOLDER')
     image_folder_name = "images"
 
-    source_file = os.path.join(source_folder, f"{document_name}.md")
+    source_file = find_file(f"{document_name}.md", source_folder)
+    if not source_file:
+        print(f"Error: Markdown file '{document_name}.md' not found.")
+        return
+
+    # Directory setup for destination
     dest_post_folder = os.path.join(destination_folder, document_name)
     dest_image_folder = os.path.join(dest_post_folder, image_folder_name)
 
@@ -30,11 +35,11 @@ def _main(document_name):
     images = re.findall(r"!\[\[(.*?)]]", content)
 
     for image_name in images:
-        source_image_path = os.path.join(source_folder, "Attachments", image_name)
+        source_image_path = find_file(image_name, source_folder)
         sanitized_image_name = _sanitize_filename(image_name)
         dest_image_path = os.path.join(dest_image_folder, sanitized_image_name)
 
-        if os.path.exists(source_image_path):
+        if source_image_path:
             shutil.copy2(source_image_path, dest_image_path)
         else:
             print(f"Warning: Image {image_name} not found in Attachments.")
@@ -50,6 +55,14 @@ def _main(document_name):
     print(f"Blog post '{document_name}' has been successfully published to the blog repo.")
 
 
+def find_file(filename, root_folder):
+    for subdir, dirs, files in os.walk(root_folder):
+        for file in files:
+            if file == filename:
+                return os.path.join(subdir, file)
+    return None
+
+
 def clear_directory(directory_path):
     for filename in os.listdir(directory_path):
         file_path = os.path.join(directory_path, filename)
@@ -63,7 +76,6 @@ def clear_directory(directory_path):
 
 
 def _update_image_links(content, image_folder_name):
-    # Regular expression to find Obsidian image links
     pattern = r"!\[\[(.*?)\]\]"
 
     def replace_func(match):
@@ -75,18 +87,15 @@ def _update_image_links(content, image_folder_name):
 
 
 def _sanitize_filename(filename):
-    """
-    Replace spaces with underscores in the filename
-    """
     return filename.replace(" ", "_")
 
 
 def _update_front_matter(content, current_date):
     front_matter_pattern = r'^(---\s+.*?\s+---)'
-    front_matter = re.search(front_matter_pattern, content, re.DOTALL).group(1)
-
-    updated_front_matter = re.sub(r'(\s+---)$', f'\nlastmod: {current_date}\ntoc: true\\1', front_matter)
-    content = content.replace(front_matter, updated_front_matter)
+    if front_matter := re.search(front_matter_pattern, content, re.DOTALL):
+        front_matter = front_matter.group(1)
+        updated_front_matter = re.sub(r'(\s+---)$', f'\nlastmod: {current_date}\ntoc: true\\1', front_matter)
+        content = content.replace(front_matter, updated_front_matter)
     return content
 
 
